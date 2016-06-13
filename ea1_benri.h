7c36ee2e0423de7b7716759ea063189f42934c2d
Sugiura,N <sgurn6i@gmail.com>
2016-06-13 16:36:36 +0900

    * ea1_benri.h: added

---
+++ ea1_benri.h
@@ -0,0 +1,16 @@
+/* ea1_benri.h    -*- C -*-
+   便利ツール
+   2016-06-13 16:31:07 Sgurn6i
+*/
+#ifndef _EA1_BENRI_H_
+#define _EA1_BENRI_H_
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define  EA1_SAFE_FREE(p) if(p) free(p)
+  
+#ifdef __cplusplus
+}
+#endif	/* C++ */
+#endif /* _EA1_BENRI_H_ */
